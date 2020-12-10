bdb58c101b011338fe70b80a78e4f33765245378
abradley2000 <aaron.s.bradley2000@gmail.com>
2020-12-10 00:10:58 -0600

    Completed part1 & 2

---
+++ Part1/index.js
@@ -0,0 +1,72 @@
+let board = [];
+
+
+
+const play = (clickedId) => {
+    const playerSpan = document.getElementById("player")
+    const clicked = document.getElementById(clickedId)
+
+    if (playerSpan.innerHTML === "X"){
+        clicked.innerHTML = "X"
+        playerSpan.innerHTML = "O"
+        board[clickedId] = "X"
+    }
+    else if (playerSpan.innerHTML === "O"){
+        clicked.innerHTML = "O"
+        playerSpan.innerHTML = "X"
+        board[clickedId] = "O"
+    }
+
+    let topLeft = board[0];
+    let topCenter = board[1];
+    let topRight = board[2];
+    let middleLeft = board[3];
+    let middleCenter = board[4];
+    let middleRight = board[5];
+    let bottomLeft = board[6];
+    let bottomCenter = board[7];
+    let bottomRight = board[8];
+
+    if (topLeft !== undefined && topLeft === topCenter && topLeft === topRight) {
+        alert(`${topLeft} is the winner!`)
+        return
+    }
+    if (middleLeft !== undefined && middleLeft === middleCenter && middleLeft === middleRight) {
+        alert(`${middleLeft} is the winner!`)
+        return
+    }
+    if (bottomLeft !== undefined && bottomLeft === bottomCenter && bottomLeft === bottomRight) {
+        alert(`${bottomLeft} is the winner!`)
+        return
+    }
+    if (topLeft !== undefined && topLeft === middleLeft && topLeft === bottomLeft) {
+        alert(`${topLeft} is the winner!`)
+        return
+    }
+    if (topCenter !== undefined && topCenter === middleCenter && topCenter === bottomCenter) {
+        alert(`${topCenter} is the winner!`)
+        return
+    }
+    if (topRight !== undefined && topRight === middleRight && topRight === bottomRight) {
+        alert(`${topRight} is the winner!`)
+        return
+    }
+    if (topLeft !== undefined && topLeft === middleCenter && topLeft === bottomRight) {
+        alert(`${topLeft} is the winner!`)
+        return
+    }
+    if (bottomLeft !== undefined && bottomLeft === middleCenter && bottomLeft === topRight) {
+        alert(`${bottomLeft} is the winner!`)
+        return
+    }
+
+    let boardFull = true;
+    for (let i = 0; i < 8; i++){
+        if (board[i] === undefined){
+            boardFull =  false;
+        }
+    }
+    if (boardFull === true){
+        alert("Cat's game")
+    }
+};
